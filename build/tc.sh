433f088feeea59b3fb268e9f9159be1af4f3ee77
ninevlc <9vlc@proton.me>
2025-09-10 18:49:08 +0300

    NEVER CODE WITH A MIGRAINE
    wtf

--- build/tc.sh
+++ build/tc.sh
@@ -3,17 +3,15 @@
 set -eu
 
 #
-# script for quickly using bootlin toolchains
+# set variables in env.
 #
 
-tc_dir="${1:-}"; [ "$tc_dir" ] && tc_dir="$(pwd)/.toolchain"
-target="${2:-mips32}"
-tc_version="${3:-musl--stable-2025.08-1}"
-
-###
+tc_target="${tc_target:-mips32}"
+tc_version="${tc_version:-musl--stable-2025.08-1}"
+[ "$tc_dir" ] || tc_dir="$(pwd)/.toolchain"
 
 tc_site="https://toolchains.bootlin.com/downloads/releases/toolchains"
-tc="$tc_site/$target/tarballs/$target--$tc_version"
+tc="$tc_site/$tc_target/tarballs/$tc_target--$tc_version"
 
 _Cln()
 { [ -d "${_tc_tmp:-}" ] && rm -rf "$_tc_tmp"; }
@@ -21,19 +19,20 @@ trap _Cln EXIT
 
 if [ ! -e "$tc_dir" ]; then
 	_tc_tmp="$(mktemp -d)"
+	wd="$(pwd)"
 	cd "$_tc_tmp"
 	>&2 echo "installing a $tc_version toolchain in $tc_dir"
 	>&2 echo "temporarily working in $_tc_tmp"
 
 	curl -o "sha256" "$tc.sha256"
-	curl -o "$target--$tc_version.tar.xz" "$tc.tar.xz"
+	curl -o "$tc_target--$tc_version.tar.xz" "$tc.tar.xz"
 
 	sha256sum -c sha256
-	tar xJvpf "$target--$tc_version.tar.xz" |while IFS= read -r _;do printf .;done
+	tar xJvpf "$tc_target--$tc_version.tar.xz" |while IFS= read -r _;do printf .;done
 	echo
 
-	mv "$target--$tc_version" "$tc_dir"
-	rm "$target--$tc_version.tar.xz" sha256
+	mv "$tc_target--$tc_version" "$tc_dir"
+	rm "$tc_target--$tc_version.tar.xz" sha256
 
 	cd "$tc_dir/bin"
 	for e in *buildroot-linux-*-*; do
@@ -46,12 +45,12 @@ if [ ! -e "$tc_dir" ]; then
 		e_new="$(printf '%s' "$e" | sed -E 's/.*linux-[a-Z]+-//g')"
 		[ -e "$e_new" ] || ln -sv "$e" "$e_new"
 	done
-	cd ../..
-
-	cd ..
+	cd "$wd"
 fi
 
 export PATH="$tc_dir/bin:$PATH"
-unset target tc_version tc_dir tc_site e_new tc e
+export PS1="$tc_version \w @ "
+echo "$PS1"
+unset tc_target tc_version tc_dir tc_site e_new wd tc e
 
 "$@"
